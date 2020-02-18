3fd31be94b5ffb1ff249845cf391b1bc0e5db156
jujujuc <55589637+jujujuc@users.noreply.github.com>
2020-02-18 12:58:19 -0500

    a

--- Lab5/projet/App.js
+++ Lab5/projet/App.js
@@ -17,30 +17,90 @@ export default class App extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      txtInput: '',
-      entryList: [
-        { id: 0, title: "This is a PlaceHolder" }
+      todoInput: '',
+      todos: [
+        { id: 0, title: "Use the app",done: false }
       ]
     }
   }
 
   addNewEntries() {
-    var newEntry = { id: this.state.entryList.length, title: this.state.txtInput }
-    this.setState({ entryList: [...this.state.entryList, newEntry] });
+    var newEntry = { id: this.state.todos.length, title: this.state.todoInput }
+    
+    
+    
+    
+    this.setState({ todos: [...this.state.todos, newEntry] });
+
+
+    /*
+    code par le prof 
+
+      this.setState({
+      todos,
+      todoInput: ''
+    });
+    */
+  }
+
+  
+  toggle(item) {
+    /* 
+    
+    A Faire:
+      fonction qui change l'etat d'un todo done: false -> true ( et inversement)
+
+    tips: La fonction Map peux etre utile
+    */
+   if(item.state.todos.done == false)
+   {
+    item = { id: item.id, title: item.title,done: true }
+
+   }
+   else{
+    item = { id: item.id, title: item.title,done: false }
+   }
+
+   //il faut juste ajuster la couleur du texte et tout devrait être good pour totalement tester
+    item.setState({todos:[...item.state.todos,item]})
+  }
+
+  removeItem(item) {
+    /* 
+
+    A faire:
+      fonction qui delete un todo (item)
+      
+    tips: La fonction Filter peux etre utile
+    
+    */
+
   }
 
   render() {
     return (
       <View style={styles.mainContainer}>
-        <Header title="Touch & Inputs demo" />
+        <Header title="TODO" />
         <InputBar
-          txtInput={this.state.txtInput}
-          onChangeText={txtInput => this.setState({ txtInput })}
+          todoInput={this.state.todoInput}
+          onChangeText={todoInput => this.setState({ todoInput })}
           action={() => this.addNewEntries()}
         />
+
+        {/*
+        code du prof
+
+
+                <InputBar 
+        addNewItem={() => this.addNewItem() }
+        textChange={todoInput => this.setState({todoInput})} 
+        todoInput={this.state.todoInput}
+        />
+
+        */}
         {
           <FlatList
-            data={this.state.entryList}
+            data={this.state.todos}
             renderItem={({ item }) => (
               <Entry
                 id={item.id}
